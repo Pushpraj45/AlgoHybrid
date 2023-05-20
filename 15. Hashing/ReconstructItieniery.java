4a86dc16a14c221d22b22c690971b1cac92c7892
Pushpraj Dubey <99044211+Pushpraj45@users.noreply.github.com>
2023-05-20 22:36:44 +0530

    Update ReconstructItieniery.java

--- 15. Hashing/ReconstructItieniery.java
+++ 15. Hashing/ReconstructItieniery.java
@@ -1,14 +1,17 @@
 import java.util.HashMap;
 
 public class ReconstructItieniery {
-
+// Itinerary means journey usually we do this type of questions with the help of graphs. 
+//     Tickets are always part of "from" and "to" so we have to find the place from where we can start our journey and end it at a required point. 
     public static String getStart(HashMap<String, String> tickets){
+        
+//         starting point -> for which from exist but to doesn't exist 
         HashMap<String, String> revMap=new HashMap<>();
 
         for (String key : tickets.keySet()){
             revMap.put(tickets.get(key), key); // value ki jagah key and vice versa
         }
-
+// find a place jo to ke andar exist nahi karta hai by this logic
         for (String key : tickets.keySet()){
             if (!revMap.containsKey(key)){
                 return key;  // starting point
@@ -20,6 +23,7 @@ public class ReconstructItieniery {
     }
     public static void main(String[] args){
         HashMap<String, String> tickets=new HashMap<>();
+//         If any other type of data structure is given convert it to maps and then solve 
 
         tickets.put("chennai", "bengaluru");
         tickets.put("mumbai", "delhi");
@@ -29,6 +33,7 @@ public class ReconstructItieniery {
         String start=getStart(tickets);
         System.out.print(start);
         for (String key : tickets.keySet()){
+//             Again start point se uski destination nikal li via iterating 
             System.out.print(" -> "+ tickets.get(start));
             start=tickets.get(start);
         }
