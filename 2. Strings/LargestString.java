1be63d04cbd2daaa5d12f10eb9cd145031dfd8bf
Pushpraj Dubey <99044211+Pushpraj45@users.noreply.github.com>
2023-05-27 21:47:03 +0530

    Update LargestString.java

--- 2. Strings/LargestString.java
+++ 2. Strings/LargestString.java
@@ -1,7 +1,7 @@
 public class LargestString {
     public static void main(String[] args){
         String fruits[] = {"apple", "mango", "banana"};
-
+// took one string of largest 
         String largest = fruits[0];
         for (int i=1; i<fruits.length; i++){
             if (largest.compareTo(fruits[i])<0){ // largest < fruits[i]
