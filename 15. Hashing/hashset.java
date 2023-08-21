7557fca7ce59eef3d046879427824513d8a740b7
Pushpraj Dubey <99044211+Pushpraj45@users.noreply.github.com>
2023-08-21 23:11:57 +0530

    Update hashset.java

--- 15. Hashing/hashset.java
+++ 15. Hashing/hashset.java
@@ -2,6 +2,7 @@ import java.util.HashSet;
 
 public class hashset {
     public static void main(String[] args){
+        // Set 
         HashSet<Integer> set=new HashSet<>();
         set.add(1);
         set.add(1);
